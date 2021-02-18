b1bdd257149e139f586765fda6808a07d42dc889
clxxu <connie.xu@nlmatics.com>
2021-02-17 22:43:54 -0500

    first commands

--- reg.py
+++ reg.py
@@ -6,6 +6,7 @@
 #----------------------------------------------------------------------------------
 
 import argparse
+import textwrap
 from os import path
 from sys import argv, stderr, exit
 from sqlite3 import connect
@@ -23,6 +24,8 @@ def main(argv):
     args = parser.parse_args()
     print(args)
 
+    wrapper = textwrap.TextWrapper(width=51)
+
     if not path.isfile(DATABASE_NAME):
         print('database reg.sqlite not found', file=stderr)
         exit(1)
@@ -30,11 +33,33 @@ def main(argv):
     try:
         connection = connect(DATABASE_NAME)
         cursor = connection.cursor()
-
-        stmtStr = 'SELECT * FROM classes'
-
-        
+        # class ID, dept, course num, area, title
+        stmtStr = 'SELECT classid, dept, coursenum, area, title ' + \
+            'FROM classes, courses, crosslistings ' + \
+            'WHERE courses.courseid = crosslistings.courseid AND classes.courseid = courses.courseid ' + \
+            'AND ' + \
+            'ORDER BY dept ASC, coursenum ASC, classid ASC'
         cursor.execute(stmtStr)
+
+        row = cursor.fetchone()
+        while row is not None:
+            rowstr = ''
+            #classid = row[0]
+            #dept = row[1]
+            #coursenum = row[2]
+            #area = row[3]
+            #title = row[4]
+            title = wrapper.wrap(row[4])
+            new_title = ''
+            for index, value in enumerate(title):
+                if (index == 0):
+                    new_title += value
+                else:
+                    new_title += '\n'+23*' ' + value
+            
+            print(f'{row[0]:5}  {row[1]:3}   {row[2].rjust(4)}   {row[3]:2} {new_title}')
+            row = cursor.fetchone()
+        
         cursor.close()
         connection.close()
 
